56dc19272837bdc1aa187ae10e544609ff4a93cd
Joao Costa <joao.costa@codacy.com>
2019-10-18 09:52:57 +0100

    feature: added set-string-keyvalue

--- ytool/test/test_yaml_path.py
+++ ytool/test/test_yaml_path.py
@@ -1,8 +1,7 @@
-import io
-import sys
+from collections import OrderedDict
+
 import pytest
 
-from collections import OrderedDict
 from ..ytool import YTool
 
 TEST_YAML = """\
@@ -22,120 +21,126 @@ some:
       another_one: "another_value"
 """
 
-def test_get_leaf_and_key_for_nested_element():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
+NESTED_TEST_YAML = """\
+dependencies:
+- name: minio
+  version: ~2.4.18
+  repository: https://kubernetes-charts.storage.googleapis.com/
+  condition: global.minio.create
+  alias: minio
+- name: core
+  version: ~2.8.2
+  repository: https://charts.codacy.com/stable
+"""
 
-    leaf, key = parser.get_leaf_and_key(data, "some.very.nested.another_one")
-    assert leaf == OrderedDict([('element', 'value'), ('another_one', 'another_value')])
-    assert key == "another_one"
+
+def test_get_leaf_and_key_for_nested_element():
+	data, parser = populate_yaml(TEST_YAML)
+	leaf, key = parser.get_leaf_and_key(data, "some.very.nested.another_one")
+	assert leaf == OrderedDict([('element', 'value'), ('another_one', 'another_value')])
+	assert key == "another_one"
+
+
+def populate_yaml(source_yaml):
+	parser = YTool()
+	parser.preserve_quotes = True
+	data = parser.load(source_yaml)
+	return data, parser
+
+
+def set_path_by_value_can_replace_value_on_same_search_kvpair():
+	data, parser = populate_yaml(NESTED_TEST_YAML)
+	leaf, key = parser.set_path_by_value(data, "dependencies", ("name", "core"), ("name", "core-io"))
+	assert leaf == OrderedDict(
+		[('name', 'core-io'), ('version', "~2.8.2"), ('repository', 'https://charts.codacy.com/stable')])
+	assert key == "name"
+
+
+def test_set_can_replace_value_on_different_kvpair():
+	data, parser = populate_yaml(NESTED_TEST_YAML)
+	leaf, key = parser.set_path_by_value(data, "dependencies", ("name", "core"), ("version", "~3.0.0"))
+	assert leaf == OrderedDict(
+		[('name', 'core'), ('version', "~3.0.0"), ('repository', 'https://charts.codacy.com/stable')])
+	assert key == "version"
 
 
 def test_get_leaf_and_key_for_non_nested_element():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    leaf, key = parser.get_leaf_and_key(data, "name")
-    assert key == "name"
-    assert leaf == OrderedDict([
-        ('apiVersion', 'v1'),
-        ('appVersion', '1.0'),
-        ('description', 'this a typical k8s chart file'),
-        ('name', 'rudder'),
-        ('version', '0.1.0'),
-        ('some', OrderedDict([
-            ('not_so_nested', 'is it'),
-            ('very', OrderedDict([
-                ('nested', OrderedDict([
-                    ('element', 'value'),
-                    ('another_one', 'another_value')]))]))
-        ]))
-    ])
+	data, parser = populate_yaml(TEST_YAML)
+	leaf, key = parser.get_leaf_and_key(data, "name")
+	assert key == "name"
+	assert leaf == OrderedDict([
+		('apiVersion', 'v1'),
+		('appVersion', '1.0'),
+		('description', 'this a typical k8s chart file'),
+		('name', 'rudder'),
+		('version', '0.1.0'),
+		('some', OrderedDict([
+			('not_so_nested', 'is it'),
+			('very', OrderedDict([
+				('nested', OrderedDict([
+					('element', 'value'),
+					('another_one', 'another_value')]))]))
+		]))
+	])
 
 
 def test_get_leaf_and_key_exception_for_wrong_plain_key():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    with pytest.raises(KeyError):
-        parser.get_leaf_and_key(data, "not_a_key")
+	data, parser = populate_yaml(TEST_YAML)
+	with pytest.raises(KeyError):
+		parser.get_leaf_and_key(data, "not_a_key")
 
 
 def test_get_leaf_and_key_exception_for_wrong_nested_key():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    with pytest.raises(KeyError):
-        parser.get_leaf_and_key(data, "some.very.wrong.element")
+	data, parser = populate_yaml(TEST_YAML)
+	with pytest.raises(KeyError):
+		parser.get_leaf_and_key(data, "some.very.wrong.element")
 
 
 def test_get_leaf_and_key_exception_for_wrong_final_key():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    with pytest.raises(KeyError):
-        parser.get_leaf_and_key(data, "some.very.nested.non_existing_element")
+	data, parser = populate_yaml(TEST_YAML)
+	with pytest.raises(KeyError):
+		parser.get_leaf_and_key(data, "some.very.nested.non_existing_element")
 
 
 def test_non_nested_value_replacement():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    test_value = 9.9
-    parser.set_path_value(data, "version", test_value)
-    assert data["version"] == test_value
+	data, parser = populate_yaml(TEST_YAML)
+	test_value = 9.9
+	parser.set_path_value(data, "version", test_value)
+	assert data["version"] == test_value
 
 
 def test_nested_value_replacement():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    test_value = "changed"
-    parser.set_path_value(data, "some.very.nested.element", test_value)
-    assert data["some"]["very"]["nested"]["element"] == test_value
+	data, parser = populate_yaml(TEST_YAML)
+	test_value = "changed"
+	parser.set_path_value(data, "some.very.nested.element", test_value)
+	assert data["some"]["very"]["nested"]["element"] == test_value
 
 
 def test_multiple_value_replacement():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
+	data, parser = populate_yaml(TEST_YAML)
+	test_value1 = 9.9
+	test_value2 = "changed"
 
-    test_value1 = 9.9
-    test_value2 = "changed"
-
-    parser.set_path_value(data, "version", test_value1)
-    parser.set_path_value(data, "some.very.nested.element", test_value2)
-    assert data["version"] == test_value1
-    assert data["some"]["very"]["nested"]["element"] == test_value2
+	parser.set_path_value(data, "version", test_value1)
+	parser.set_path_value(data, "some.very.nested.element", test_value2)
+	assert data["version"] == test_value1
+	assert data["some"]["very"]["nested"]["element"] == test_value2
 
 
 def test_format_is_preserved():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
-
-    assert parser.dump(data) == TEST_YAML
+	data, parser = populate_yaml(TEST_YAML)
+	assert parser.dump(data) == TEST_YAML
 
 
 def test_format_is_preserved_when_values_change():
-    parser = YTool()
-    parser.preserve_quotes = True
-    data = parser.load(TEST_YAML)
+	data, parser = populate_yaml(TEST_YAML)
+	test_value = "changed"
+	original_value = "value"
+	test_string = '      element: "value"'
 
-    test_value = "changed"
-    original_value = "value"
-    test_string = '      element: "value"'
+	parser.set_path_value(data, "some.very.nested.element", test_value)
 
-    parser.set_path_value(data, "some.very.nested.element", test_value)
-
-    diff = [(x1,x2) for (x1,x2) in zip(TEST_YAML.split('\n'), parser.dump(data).split('\n')) if x1 != x2]
-    assert len(diff) == 1
-    assert diff[0][0] == test_string
-    assert diff[0][1] == test_string.replace(original_value, test_value, 1)
+	diff = [(x1, x2) for (x1, x2) in zip(TEST_YAML.split('\n'), parser.dump(data).split('\n')) if x1 != x2]
+	assert len(diff) == 1
+	assert diff[0][0] == test_string
+	assert diff[0][1] == test_string.replace(original_value, test_value, 1)
--- ytool/ytool.py
+++ ytool/ytool.py
@@ -3,87 +3,125 @@ from ruamel.yaml.compat import StringIO
 
 
 class YTool(YAML):
-    """
-    YTool is a simple extension of ruamel.yaml useful for changing
-    values when using paths to refer to nested objects.
-    """
+	"""
+	YTool is a simple extension of ruamel.yaml useful for changing
+	values when using paths to refer to nested objects.
+	"""
 
-    def get_leaf_and_key(self, data, path, delimiter="."):
-        """
-        Find leaf in the yaml tree containing a key extracted from a path
-        For instance, when the yaml data is 
+	def get_leaf_and_key(self, data, path, delimiter="."):
+		"""
+		Find leaf in the yaml tree containing a key extracted from a path
+		For instance, when the yaml data is
 
-        ```
-        this:
-            nested:
-                object1: value1
-                object2: value2
-        ```
-        and this function is called with path=`this.nested.object1` it returns
-        `OrderedDict([(object1, value1)`, (object2, value2)]) and `object1`
-
-
-        args:
-            data (OrderedDict): ruamel parsed yaml data
-            path (str): path to key to search for
-            delimiter (str): path delimiter. Default is '.'
-        
-        returns:
-            leaf, key (OrderedDict, str): the `leaf` in the yaml tree containing `key`
-        
-        raises:
-            KeyError: if any of the elements of the path is not a valid key in the corresponding yaml leaf
-        """
-        keys = path.split(delimiter)
-
-        # find the innermost dict holding the key we want to change
-        leaf = data
-        for key in keys[:-1]:
-            if key in leaf:
-                leaf = leaf[key]
-            else:
-                raise KeyError(f"Key '{key}' not found in '{leaf}'")
-
-        # get last key, checking if it is valid
-        last_key = keys[-1]
-        if last_key in leaf:
-            return leaf, last_key
-        else:
-            raise KeyError(f"Key '{last_key}' not found in '{leaf}'")
+		```
+		this:
+			nested:
+				object1: value1
+				object2: value2
+		```
+		and this function is called with path=`this.nested.object1` it returns
+		`OrderedDict([(object1, value1)`, (object2, value2)]) and `object1`
 
 
-    def set_path_value(self, data, path, value, delimiter="."):
-        """
-        Set a value of `data` for a given path
+		args:
+			data (OrderedDict): ruamel parsed yaml data
+			path (str): path to key to search for
+			delimiter (str): path delimiter. Default is '.'
 
-        args:
-            data (OrderedDict): ruamel parsed yaml data
-            path (str): path to the key to change
-            value (Any): value to assign to the key
-            delimiter (str): path delimiter. Default is '.'
-        """
-        leaf, key = self.get_leaf_and_key(data, path, delimiter)
-        leaf[key] = value
+		returns:
+			leaf, key (OrderedDict, str): the `leaf` in the yaml tree containing `key`
 
-    
-    def dump(self, data, stream=None, **kw):
-        """
-        Wrapper around the ruamel dump function which writes to a
-        string stream if no stream is provided. Useful for testing.
-        Taken from the ruamel documentation:
-            https://yaml.readthedocs.io/en/latest/example.html#output-of-dump-as-a-string
+		raises:
+			KeyError: if any of the elements of the path is not a valid key in the corresponding yaml leaf
+		"""
+		keys = path.split(delimiter)
 
-        args:
-            data (OrderedDict): ruamel parsed yaml data
-            stream (Union[Path, StreamType]): stream where to dump the data
-            **kw (Any=Any): ruamel dump flags and respective values
-        """
-        inefficient = False
-        if stream is None:
-            inefficient = True
-            stream = StringIO()
+		# find the innermost dict holding the key we want to change
+		leaf = data
+		for key in keys[:-1]:
+			if key in leaf:
+				leaf = leaf[key]
+			else:
+				raise KeyError(f"Key '{key}' not found in '{leaf}'")
 
-        YAML.dump(self, data, stream, **kw)
+		# get last key, checking if it is valid
+		last_key = keys[-1]
+		if last_key in leaf:
+			return leaf, last_key
+		else:
+			raise KeyError(f"Key '{last_key}' not found in '{leaf}'")
 
-        if inefficient:
-            return stream.getvalue()
+	def set_path_by_value(self, data, path, searchKVPair, replaceKVPair):
+		"""
+				Find leaf in the yaml tree containing a key-value pair extracted from a path
+				For instance, when the yaml data is
+
+				```
+				this:
+					nested:
+						object1: value1
+						object2: value2
+
+				this:
+					nested:
+					- key1: value1
+					  key2: value2
+					- key1: value3
+					  key2: value4
+				```
+				and this function is called with path=`this.nested (key1, value3) (key2, value4)` it returns
+				`OrderedDict([(key1, value3)`, (key2, value4)]) and `key2`
+
+
+				args:
+					data (OrderedDict): ruamel parsed yaml data
+					path (str): path to key to search for
+					searchKVPair (tuple): the key and value pair for searching for an element
+					replaceKVPair (tuple): the key and value pair with new values to replace on the element
+
+				returns:
+					leaf, key (OrderedDict, str): the `leaf` in the yaml tree containing `key` if found.
+					tree, key (OrderedDict, str): if the `leaf` could not be found.
+				"""
+		tree, key = self.get_leaf_and_key(data, path)
+		for leaf in tree[key]:
+			if leaf[searchKVPair[0]] == searchKVPair[1]:
+				leaf[replaceKVPair[0]] = replaceKVPair[1]
+				return leaf, replaceKVPair[0]
+
+		return tree, key
+
+	def set_path_value(self, data, path, value, delimiter="."):
+		"""
+		Set a value of `data` for a given path
+
+		args:
+			data (OrderedDict): ruamel parsed yaml data
+			path (str): path to the key to change
+			value (Any): value to assign to the key
+			delimiter (str): path delimiter. Default is '.'
+		"""
+		leaf, key = self.get_leaf_and_key(data, path, delimiter)
+		leaf[key] = value
+
+	def dump(self, data, stream=None, **kw):
+		"""
+		Wrapper around the ruamel dump function which writes to a
+		string stream if no stream is provided. Useful for testing.
+		Taken from the ruamel documentation:
+			https://yaml.readthedocs.io/en/latest/example.html#output-of-dump-as-a-string
+
+		args:
+			data (OrderedDict): ruamel parsed yaml data
+			stream (Union[Path, StreamType]): stream where to dump the data
+			**kw (Any=Any): ruamel dump flags and respective values
+		"""
+		inefficient = False
+		if stream is None:
+			inefficient = True
+			stream = StringIO()
+
+		YAML.dump(self, data, stream, **kw)
+
+		if inefficient:
+			return stream.getvalue()
